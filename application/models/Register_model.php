81907118864b059c42dd1b89de28de2eebcbf784
maurisaarimbi <caca.maurisa@gmail.com>
2023-10-10 21:27:22 +0700

    add register admin

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -89,6 +89,40 @@ class Register_model extends MY_Model
 
         return true;
     }
+
+    public function runAdmin($input)
+    {
+        $data = [
+            'name'      => $input->name,
+            'email'     => strtolower($input->email),
+            'password'  => hashEncrypt($input->password),
+            'role'      => 'admin',
+            'dob'       => $input->dob,
+            'gender'    => $input->gender,
+            'address'   => $input->address,
+            'city'      => $input->city,
+            'phone'     => $input->phone,
+            'paypal'    => $input->paypal
+        ];
+
+        $user = $this->create($data);   // Insert database
+
+        $sess_data = [
+            'id'        => $user,
+            'name'      => $data['name'],
+            'email'     => $data['email'],
+            'role'      => $data['role'],
+            'is_login'  => true,
+            'address'   => $data['address'],
+            'city'      => $data['city'],
+            'phone'     => $data['phone'],
+            'paypal'    => $data['paypal']
+        ];
+
+        $this->session->set_userdata($sess_data);
+
+        return true;
+    }
 }
 
 /* End of file Register_model.php */
